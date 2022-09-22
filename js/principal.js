94b6ad518505d7b73c6884b6db23a88ae1592744
HunterAdv <gustavopaulody@gmail.com>
2022-09-22 14:33:40 -0300

    - Adicionando form para adicionar paciente no html - Configurando id nome no css - Adicionando evento de criar paciente no botão

--- js/principal.js
+++ js/principal.js
@@ -31,7 +31,42 @@ for (let i = 0; i < pacientes.length; i++) {
     }
 
     if (alturaEValida && pesoEValido) {
-        let imc = peso / (altura * altura);
+        var imc = peso / (altura * altura);
         tdImc.textContent = imc.toFixed(2);
     }
 }
+
+let botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+    
+    let form = document.querySelector("#form-adiciona");
+
+    let nome = form.nome.value;
+    let peso = form.peso.value;
+    let altura = form.altura.value;    
+    let gordura = form.gordura.value;
+
+    let pacienteTr = document.createElement("tr"); 
+    
+    let nomeTd = document.createElement("td");
+    let pesoTd = document.createElement("td");
+    let alturaTd = document.createElement("td");
+    let gorduraTd = document.createElement("td");
+    let imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = imc;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);    
+
+    let tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+});
